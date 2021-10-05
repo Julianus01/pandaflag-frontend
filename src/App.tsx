8c57714634cd58bb129dd63218967beff1bfcc70
Iulian Crisan <i.crisan@epilot.cloud>
2021-10-05 21:23:15 +0300

    Auth0 login/logout functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import { useAuth0 } from '@auth0/auth0-react'
 
 function App() {
+  const { user, loginWithRedirect, logout } = useAuth0()
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
+
+        {user?.name && <p>{user.name}</p>}
+
+        {!Boolean(user) && (
+          <p onClick={loginWithRedirect} className="App-link">
+            Login
+          </p>
+        )}
+
+        <p
+          onClick={() => logout({ returnTo: window.location.origin })}
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+          Logout
+        </p>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+import { Auth0Provider } from '@auth0/auth0-react'
+
+const DOMAIN = 'dev-rfaxp9q7.eu.auth0.com'
+const CLIENT_ID = 'aBR3I3KXiLF973VWp7aVdsY7cD6KOg8H'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain={DOMAIN}
+      clientId={CLIENT_ID}
+      redirectUri={window.location.origin}
+      useRefreshTokens
+      cacheLocation="localstorage"
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
