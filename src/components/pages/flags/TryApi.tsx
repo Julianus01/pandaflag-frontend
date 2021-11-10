c1c3bec70e09dd49ce5afa62b439af5e8a1d9696
Iulian Crisan <i.crisan@epilot.cloud>
2021-11-10 17:09:24 +0000

    Switched smartlaunch endpoint with pandaflag endpoint

--- src/components/pages/flags/TryApi.tsx
+++ src/components/pages/flags/TryApi.tsx
@@ -153,7 +153,7 @@ function TryApi({ flags, isOpen }: IProps) {
 
           <Text mb={4}>
             <Code>
-              https://smartlaunch-api-dev.ey.r.appspot.com/api/v1/
+              https://pandaflag-api-dev.ey.r.appspot.com/api/v1/
               <Box color="orange.500" as="span">
                 API_KEY
               </Box>
@@ -220,7 +220,7 @@ function TryApi({ flags, isOpen }: IProps) {
 
           <CodeContainer mb={4}>
             <Code display="flex" flexWrap="wrap">
-              fetch('https://smartlaunch-api-dev.ey.r.appspot.com/api/v1/
+              fetch('https://pandaflag-api-dev.ey.r.appspot.com/api/v1/
               {
                 <Box color="orange.500" as="span">
                   {configuration.project?.apiKey}
