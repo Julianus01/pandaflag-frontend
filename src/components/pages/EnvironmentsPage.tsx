f2b752e3f4c7557099370606d6adfee9c36e12db
Iulian Crisan <iuliancrisan01@gmail.com>
2021-11-26 00:33:51 +0200

    Added quota alert for Environemnts page

--- src/components/pages/EnvironmentsPage.tsx
+++ src/components/pages/EnvironmentsPage.tsx
@@ -1,19 +1,31 @@
-import { Heading } from '@chakra-ui/react'
+import { Heading, Alert, AlertIcon } from '@chakra-ui/react'
 import { IEnvironment } from 'api/ProjectsApi'
 import TableContainer from 'components/shared/TableContainer'
 import BoxedPage from 'components/styles/BoxedPage'
 import { useSelector } from 'react-redux'
 import { IStoreState } from 'redux/store'
+import { PricingUtils } from 'utils/PricingUtils'
 import EnvironmentsTable from './environments/EnvironmentsTable'
+
+const Quota = PricingUtils.getQuota()
 
 function EnvironmentsPage() {
   const environments = useSelector((state: IStoreState) => state.configuration.project?.environments)
+
+  const isEnvironmentsQuotaReached = environments?.length === Quota.environments
 
   return (
     <BoxedPage>
       <Heading mb={10} flex={1} as="h3" size="lg">
         Environments
       </Heading>
+
+      {isEnvironmentsQuotaReached && (
+        <Alert fontSize="sm" borderRadius="md" mb="6" status="info">
+          <AlertIcon w="4" h="4" />
+          You've reached the limit for number of projects.
+        </Alert>
+      )}
 
       <TableContainer>
         <EnvironmentsTable environments={environments as IEnvironment[]} />
