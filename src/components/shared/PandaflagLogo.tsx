ea2d1fa5b006304cd0b98d485da736e836f844bc
Iulian Crisan <iuliancrisan01@gmail.com>
2021-11-26 16:50:46 +0200

    Logo based on environment

--- src/components/shared/PandaflagLogo.tsx
+++ src/components/shared/PandaflagLogo.tsx
@@ -1,9 +1,17 @@
 import { Heading, HeadingProps } from '@chakra-ui/react'
 
+function getTitle() {
+  if (process.env.NODE_ENV === 'development') {
+    return 'pandaflag DEV'
+  }
+
+  return 'pandaflag'
+}
+
 function PandaflagLogo(props: HeadingProps) {
   return (
     <Heading fontWeight="semibold" as="h4" size="md" {...props}>
-      pandaflag
+      {getTitle()}
     </Heading>
   )
 }
