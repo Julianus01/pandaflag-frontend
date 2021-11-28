f53b8d97952c6b74467fbef4988b9aa520d3d6f2
Iulian Crisan <iuliancrisan01@gmail.com>
2021-11-28 11:22:03 +0200

    Added GA tracking for development also, for testing

--- src/components/shared/GaTracking.tsx
+++ src/components/shared/GaTracking.tsx
@@ -1,16 +1,13 @@
 import { useLocation } from 'react-router'
 import ReactGA from 'react-ga'
 import { useEffect } from 'react'
-import CommonUtils, { NodeEnvironment } from 'utils/CommonUtils'
 
 function GaTracking() {
   const location = useLocation()
 
   useEffect(() => {
-    if (CommonUtils.isNodeEnvironment(NodeEnvironment.production)) {
-      ReactGA.set({ page: location.pathname + location.search })
-      ReactGA.pageview(location.pathname + location.search)
-    }
+    ReactGA.set({ page: location.pathname + location.search })
+    ReactGA.pageview(location.pathname + location.search)
   }, [location])
 
   return null
