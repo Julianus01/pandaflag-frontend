c5526d86649a8c4a20166313acd871aa885f8e74
Julian01 <iuliancrisan01@gmail.com>
2022-05-20 18:05:03 +0200

    Endpoint update

--- src/api/BaseApi.ts
+++ src/api/BaseApi.ts
@@ -6,7 +6,7 @@ async function getFlags(environmentName: string): Promise<IFlag[]> {
   const project = store.getState().configuration.project as IProject
 
   const response = await fetch(
-    `${process.env.REACT_APP_PANDAFLAG_API_URL as string}/${project.apiKey}/${environmentName}/`
+    `${process.env.REACT_APP_PANDAFLAG_API_URL as string}/${project.apiKey}/${environmentName}`
   )
 
   const responseJSON = await response.json()
