76c8f53377e6c23d8566a0c59c3e4297be09cd95
Julian01 <iuliancrisan01@gmail.com>
2022-11-09 21:52:30 +0100

    AcceptInvitationLoginPage now works properly

--- src/components/pages/AcceptInvitationLoginPage.tsx
+++ src/components/pages/AcceptInvitationLoginPage.tsx
@@ -13,11 +13,9 @@ import {
   Icon,
 } from '@chakra-ui/react'
 import styled from 'styled-components/macro'
-import { ChangeEvent, useState, KeyboardEvent } from 'react'
+import { ChangeEvent, useState, KeyboardEvent, useEffect } from 'react'
 import { useTemporaryMessage } from 'hooks/common/useTemporaryMessage'
-import { useQuery } from 'react-query'
-import { ApiQueryId } from 'api/ApiQueryId'
-import { NavLink, Redirect, useParams } from 'react-router-dom'
+import { NavLink, Redirect, useHistory, useParams } from 'react-router-dom'
 import OrganizationsApi, { IOrganization } from 'api/OrganizationsApi'
 import { FiMail, FiKey } from 'react-icons/fi'
 import { ICredentials } from './LoginPage'
@@ -25,11 +23,15 @@ import * as yup from 'yup'
 import AuthApi from 'api/AuthApi'
 import UsersApi, { IMemberRelation, MemberType } from 'api/UsersApi'
 import { UserCredential } from '@firebase/auth'
+import PandaflagLogo from 'components/shared/PandaflagLogo'
 import ThemeButton from 'theme/ThemeButton'
 import Section from 'components/styles/Section'
 import RoutePage from 'components/routes/RoutePage'
 import { PricingUtils } from 'utils/PricingUtils'
-import PandaflagLogo from 'components/shared/PandaflagLogo'
+import { FaGoogle } from 'react-icons/fa'
+import store from 'redux/store'
+import { useQueryClient } from 'react-query'
+import { ApiQueryId } from 'api/ApiQueryId'
 
 const Quota = PricingUtils.getQuota()
 
@@ -47,25 +49,53 @@ const ValidationSchema = yup.object().shape({
   Email: yup.string().email().required(),
 })
 
+// Use old api instead of react-query because this route needs to be logged out
+// in order for "continue with google" to work
+// For google, we need to login the user to get credentials, then based on them,
+// continue and update organization with new member or logout
+// When logging out, we have queryClient.resetQueries() in App.tsx
+// Which resets the query and
+function useOrganizationById(orgId: string) {
+  const [organization, setOrganization] = useState<IOrganization | undefined>(undefined)
+  const [isLoading, setIsLoading] = useState<boolean>(true)
+
+  useEffect(() => {
+    async function getOrganization() {
+      setIsLoading(true)
+      const data = await OrganizationsApi.getOrganizationById(orgId)
+
+      setOrganization(data)
+      setIsLoading(false)
+    }
+
+    getOrganization()
+  }, [orgId])
+
+  return { organization, isLoading }
+}
+
 interface IParams {
   orgId: string
 }
 
 function AcceptInvitationLoginPage() {
+  const history = useHistory()
   const params = useParams<IParams>()
   const temporaryMessage = useTemporaryMessage()
+  const queryClient = useQueryClient()
   const [form, setForm] = useState<ICredentials>(DefaultCredentials)
   const [isLoginLoading, setIsLoginLoading] = useState<boolean>(false)
+  const [isGoogleLoading, setIsGoogleLoading] = useState<boolean>(false)
+  const { organization, isLoading } = useOrganizationById(params.orgId)
 
-  const organizationQuery = useQuery(
-    ApiQueryId.getOrganizationForInvitation,
-    () => OrganizationsApi.getOrganizationById(params.orgId),
-    {
-      enabled: Boolean(params.orgId),
+  // Log user out for this page
+  useEffect(() => {
+    const user = store.getState().auth.user
+
+    if (user) {
+      AuthApi.logout()
     }
-  )
-
-  const organization = organizationQuery.data
+  }, [])
 
   const isMembersQuotaReached = (organization?.members?.length as number) >= Quota.members
 
@@ -77,11 +107,11 @@ function AcceptInvitationLoginPage() {
 
   function onKeyDown(event: KeyboardEvent<HTMLInputElement>) {
     if (event.key === 'Enter') {
-      onRegister()
+      onLogin()
     }
   }
 
-  async function onRegister() {
+  async function onLogin() {
     try {
       temporaryMessage.hideMessage()
       const validatedForm = await ValidationSchema.validate({
@@ -109,6 +139,37 @@ function AcceptInvitationLoginPage() {
     }
   }
 
+  async function onLoginWithGoogleCredential() {
+    try {
+      setIsGoogleLoading(true)
+      temporaryMessage.hideMessage()
+      const userCredential = await AuthApi.loginWithGoogleCredential()
+
+      const userHasOrganization = await UsersApi.doesUserHaveOrganization(userCredential.user.email as string)
+
+      if (userHasOrganization) {
+        AuthApi.logout()
+        setIsGoogleLoading(false)
+        temporaryMessage.showMessage(
+          'This account is already part of an organization. Leave that organization in order to join this one'
+        )
+
+        return
+      }
+
+      await updateOrganizationWithNewUser(userCredential)
+
+      // Reset getOrganization so that 'isLoading' from OrganizationRoute
+      // triggers, and the UI doesn't flinch with wrong screen
+      queryClient.resetQueries(ApiQueryId.getOrganization)
+      history.push(RoutePage.members())
+    } catch (err) {
+      const error = err as Error
+      temporaryMessage.showMessage(error.message)
+      setIsGoogleLoading(false)
+    }
+  }
+
   async function updateOrganizationWithNewUser(userCredential: UserCredential) {
     const organizationWithNewMember = addMemberToOrganization(organization as IOrganization, {
       id: userCredential.user.uid,
@@ -118,9 +179,15 @@ function AcceptInvitationLoginPage() {
     await OrganizationsApi.updateOrganization(organizationWithNewMember)
   }
 
-  if (organizationQuery.isLoading) {
+  if (isLoading) {
     return (
       <Container>
+        <Box display="flex" justifyContent="center">
+          <a href={process.env.REACT_APP_PANDAFLAG_APP_URL as string}>
+            <PandaflagLogo mx="auto" mt={6} />
+          </a>
+        </Box>
+
         <Content>
           <ContentBox>
             <Spinner mb={6} />
@@ -149,7 +216,7 @@ function AcceptInvitationLoginPage() {
       <Content>
         <ContentBox>
           <Heading mb={2} as="h2" size="lg">
-            Login in {organization?.name}
+            Log in in {organization?.name}
           </Heading>
 
           <Text color="gray.500" mb={4}>
@@ -164,7 +231,7 @@ function AcceptInvitationLoginPage() {
 
             <Input
               value={form.email}
-              disabled={organizationQuery.isLoading}
+              disabled={isLoading}
               onKeyDown={onKeyDown}
               onChange={onInputChange('email')}
               variant="filled"
@@ -177,7 +244,7 @@ function AcceptInvitationLoginPage() {
 
             <Input
               value={form.password}
-              disabled={organizationQuery.isLoading}
+              disabled={isLoading}
               onKeyDown={onKeyDown}
               onChange={onInputChange('password')}
               variant="filled"
@@ -191,7 +258,7 @@ function AcceptInvitationLoginPage() {
             isLoading={isLoginLoading}
             disabled={isLoginLoading}
             loadingText="Log in & Join"
-            onClick={onRegister}
+            onClick={onLogin}
             colorScheme="primary"
           >
             Log in & Join
@@ -204,6 +271,18 @@ function AcceptInvitationLoginPage() {
           )}
         </ContentBox>
       </Content>
+
+      <Box mt={6} mx="auto">
+        <Button
+          isLoading={isGoogleLoading}
+          disabled={isLoginLoading}
+          leftIcon={<FaGoogle />}
+          width="100%"
+          onClick={onLoginWithGoogleCredential}
+        >
+          continue with Google
+        </Button>
+      </Box>
 
       <Text mt={24} mx="auto" color="gray.500">
         Don't have an account?{' '}
