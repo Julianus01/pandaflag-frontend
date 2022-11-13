c3be8857baa397e7a8b99e248c756e38ed68fb9b
Julian01 <iuliancrisan01@gmail.com>
2022-11-13 21:26:26 +0100

    Confetti for success purchase

--- src/components/pages/organization/OrganizationPricingUpgradeSuccessAlert.tsx
+++ src/components/pages/organization/OrganizationPricingUpgradeSuccessAlert.tsx
@@ -3,34 +3,41 @@ import RoutePage from 'components/routes/RoutePage'
 import useQueryParam from 'hooks/routing/useQueryParam'
 import { QueryParam } from 'hooks/routing/useQueryParams'
 import { useHistory } from 'react-router-dom'
+import { useWindowSize } from 'react-use'
+import Confetti from 'react-confetti'
+import { useState } from 'react'
 
 function OrganizationPricingUpgradeSuccessAlert() {
+  const { width, height } = useWindowSize()
   const history = useHistory()
-  const upgradeSuccess = useQueryParam(QueryParam.success)
+  const upgradeSuccess = useQueryParam(QueryParam.success) === 'true'
   const productId = useQueryParam(QueryParam.productId)
+  const [showConfetti] = useState<boolean>(upgradeSuccess)
 
   function onClose() {
     history.replace(RoutePage.organization())
   }
 
-  if (upgradeSuccess !== 'true') {
-    return null
-  }
-
   return (
-    <Alert borderRadius="md" mb={4} status="success">
-      <AlertIcon />
+    <>
+      {showConfetti && <Confetti numberOfPieces={300} recycle={false} width={width} height={height} />}
 
-      <Box>
-        <AlertTitle>Success upgrade for {productId}</AlertTitle>
+      {upgradeSuccess && (
+        <Alert borderRadius="md" mb={4} status="success">
+          <AlertIcon />
 
-        <AlertDescription>
-          Your application has been received. We will review your application and respond within the next 48 hours.
-        </AlertDescription>
-      </Box>
+          <Box>
+            <AlertTitle>Success upgrade for {productId}</AlertTitle>
 
-      <CloseButton alignSelf="flex-start" position="relative" right={-1} top={-1} onClick={onClose} />
-    </Alert>
+            <AlertDescription>
+              Your application has been received. We will review your application and respond within the next 48 hours.
+            </AlertDescription>
+          </Box>
+
+          <CloseButton alignSelf="flex-start" position="relative" right={-1} top={-1} onClick={onClose} />
+        </Alert>
+      )}
+    </>
   )
 }
 
